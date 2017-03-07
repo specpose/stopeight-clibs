d766a2f34d48139b29e4e461054671d636c09999
Fassio Blatter <fassio@specpose.com>
2017-03-07 12:35:20 +0000

    REFACTORING structs with category

--- shared-include/shared_types.h
+++ shared-include/shared_types.h
@@ -2,19 +2,33 @@
 #ifndef SHARED_TYPES_H
 #define SHARED_TYPES_H
 namespace sp {
-	/*template<typename T> class fixpoint : timecode<T> {
-		fixpoint() {};
-		//fixpoint(timecode&& point) :*this{point}
-		~fixpoint() override {
-			throw std::exception("fixpoints refer to timecode data and can not be deleted.");
-		};
+
+	enum class tctype
+	{
+		EMPTY,
+		TURN,
+		CORNER
 	};
-	template<typename T> class timecode : public std::pair<T,T> {
+	template<typename T> struct timecode : public std::pair<T,T> {
 	public:
-		timecode() {};
-		virtual ~timecode() = 0;
-	};*/
-	using element = std::pair<double, double>;
+		using std::pair<T, T>::pair;
+		using timecode_types = typename tctype;
+		timecode_types category{sp::tctype::EMPTY};
+		//virtual ~timecode() = 0;
+	};
+	template<typename T> struct turn : timecode<T> {
+	public:
+		using timecode_types = typename timecode::timecode_types;
+		timecode_types category{ sp::tctype::TURN };
+		
+		//fixpoint() {};
+		//fixpoint(timecode&& point) :*this{point}
+		//~fixpoint() override {
+		//	throw std::exception("fixpoints refer to timecode data and can not be deleted.");
+		//};
+	};
+	using element = timecode<double>;
+	//using element = std::pair<double, double>;
 	using result = std::pair<std::vector<element>, std::vector<element>>;
 	//using result = std::vector<element>;
 	double static _angle(double diff) { return 300 * asin(diff); };
