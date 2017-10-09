635c2308dc257963013d37aee15a221f188b4278
Fassio Blatter <fassio@specpose.com>
2017-10-09 02:21:32 +0100

    FIXED make grapher-wrappers

--- grapher-wrappers/interfacepython.cpp
+++ grapher-wrappers/interfacepython.cpp
@@ -1,3 +1,8 @@
 // Copyright (C) 2017 Fassio Blatter
 // GNU Lesser General Public License, version 2.1
+#include <pybind11/pybind11.h>
 
+PYBIND11_PLUGIN(grapher) {
+  pybind11::module m("grapher", "Converting time-based series to Vector Graph");
+  return m.ptr();
+}
