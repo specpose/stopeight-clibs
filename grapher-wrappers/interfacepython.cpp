bfbfd7f77518b84c1f97f10112cc8ac17962fdf9
Fassio Blatter <fassio@specpose.com>
2017-10-10 02:07:19 +0100

    ADDED header only cmake stopeight-clibs

--- grapher-wrappers/interfacepython.cpp
+++ grapher-wrappers/interfacepython.cpp
@@ -1,6 +1,7 @@
 // Copyright (C) 2017 Fassio Blatter
 // GNU Lesser General Public License, version 2.1
 #include <pybind11/pybind11.h>
+#include <stopeight-clibs/algo.h>
 
 PYBIND11_PLUGIN(grapher) {
   pybind11::module m("grapher", "Converting time-based series to Vector Graph");
