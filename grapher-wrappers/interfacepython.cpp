a73a6f5d553f1f8a995e58bb2a15938220cf70e5
Fassio Blatter <fassio@specpose.com>
2017-10-11 18:39:32 +0100

    ADDED non- or overloaded stl style function to pybind grapher-wrappers

--- grapher-wrappers/interfacepython.cpp
+++ grapher-wrappers/interfacepython.cpp
@@ -2,8 +2,21 @@
 // GNU Lesser General Public License, version 2.1
 #include <pybind11/pybind11.h>
 #include <stopeight-clibs/algo.h>
+#include <stopeight-clibs/shared_types.h>
+#include "dummy.h"
+#include <stopeight-clibs/angle_functions.h>
+using fexec = const dummy;
 
-PYBIND11_PLUGIN(grapher) {
-  pybind11::module m("grapher", "Converting time-based series to Vector Graph");
-  return m.ptr();
+using namespace pybind11;
+using namespace grapher;
+
+PYBIND11_MODULE(grapher, m){
+    class_<samples_To_VG>(m,"VectorGraph")
+            .def(init<int,double,std::vector<int>>())
+//            .def("exec",overload_cast<fexec&, vector_single<double>,vector_single<double>,vector_pair<double>,angle::angle&>(&samples_To_VG::operator()<fexec,vector_single<double>,vector_pair<double>,angle::angle>))
+//            .def("exec",overload_cast<fexec&, vector_single<double>,vector_single<double>,vector_pair<double>,angle::angle&>(&samples_To_VG::operator()))
+//            .def("exec",(void (samples_To_VG::*)(fexec&, vector_single<double>,vector_single<double>,vector_pair<double>,angle::angle&)) &samples_To_VG::operator()<fexec,vector_single<double>,vector_pair<double>,angle::angle> )
+//            .def("exec",(void (samples_To_VG::*)(fexec&, vector_single<double>,vector_single<double>,vector_pair<double>,angle::angle&)) &samples_To_VG::operator() )
+            .def("exec", &samples_To_VG::operator()<fexec,vector_single<double>,vector_pair<double>,angle::angle> )
+            ;
 }
