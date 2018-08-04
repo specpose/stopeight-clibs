f0052728bd0525738ae4bc6168e7a76e25d4a405
Fassio Blatter <fassio@specpose.com>
2018-08-05 00:33:48 +0100

    ADDED grapher module stubs

--- grapher-wrappers/interfacepython.cpp
+++ grapher-wrappers/interfacepython.cpp
@@ -1,6 +1,7 @@
 // Copyright (C) 2017 Fassio Blatter
 // GNU Lesser General Public License, version 2.1
 #include <pybind11/pybind11.h>
+//#include <pybind11/stl.h>
 #include <stopeight-clibs/algo.h>
 #include <stopeight-clibs/shared_types.h>
 #include "dummy.h"
@@ -10,6 +11,10 @@ using fexec = const dummy;
 using namespace pybind11;
 using namespace grapher;
 
+//converter(std::vector& v){
+//    return
+//}
+
 //new >2.2
 //PYBIND11_MODULE(grapher, m){
 //old
@@ -17,6 +22,7 @@ PYBIND11_PLUGIN(grapher){
     pybind11::module m("grapher","");
 //old
     class_<samples_To_VG>(m,"VectorGraph")
+//    class_<samples_To_VG>(m,"VectorGraphConverter")
             .def(init<int,double,std::vector<int>>())
 //            .def("exec",overload_cast<fexec&, vector_single<double>,vector_single<double>,vector_pair<double>,angle::angle&>(&samples_To_VG::operator()<fexec,vector_single<double>,vector_pair<double>,angle::angle>))
 //            .def("exec",overload_cast<fexec&, vector_single<double>,vector_single<double>,vector_pair<double>,angle::angle&>(&samples_To_VG::operator()))
