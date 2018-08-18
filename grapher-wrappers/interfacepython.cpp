9a90e79d309ea1e21d0f1c75d58965d204f72cf5
Fassio Blatter <fassio@specpose.com>
2018-08-18 19:01:48 +0100

    FIXED grapher interfacepython stl vector

--- grapher-wrappers/interfacepython.cpp
+++ grapher-wrappers/interfacepython.cpp
@@ -1,28 +1,48 @@
 // Copyright (C) 2017 Fassio Blatter
 // GNU Lesser General Public License, version 2.1
 #include <pybind11/pybind11.h>
-//#include <pybind11/stl.h>
 #include <stopeight-clibs/algo.h>
 #include <stopeight-clibs/shared_types.h>
 #include "dummy.h"
 #include <stopeight-clibs/angle_functions.h>
+
+//grapher
+#include <pybind11/stl_bind.h>
+//#include <pybind11/numpy.h>
+PYBIND11_MAKE_OPAQUE(std::vector<int16_t>);
+#include <stopeight-clibs/grapher.h>
+
+//algo
+//#include <pybind11/stl.h>
+
 using fexec = const dummy;
 
 using namespace pybind11;
 using namespace grapher;
 
-//converter(std::vector& v){
-//    return
-//}
-
 //new >2.2
-//PYBIND11_MODULE(grapher, m){
+PYBIND11_MODULE(grapher, m){
 //old
-PYBIND11_PLUGIN(grapher){
-    pybind11::module m("grapher","");
+//PYBIND11_PLUGIN(grapher){
+//    pybind11::module m("grapher","");
 //old
-    class_<samples_To_VG>(m,"VectorGraph")
-//    class_<samples_To_VG>(m,"VectorGraphConverter")
+
+//    m.def("cast_VectorInt16",[](){return std::vector<int16_t>{1};});
+    bind_vector<std::vector<int16_t>>(m,"VectorInt16",buffer_protocol());
+/*    class_<speczilla::Buffer<int16_t>>(m,"Buffer",buffer_protocol())
+            .def_buffer([](speczilla::Buffer<int16_t> &b) -> buffer_info{
+                return buffer_info{
+                    b.buf,
+                    sizeof(int16_t),
+                    format_descriptor<int16_t>::format(),
+                    1,
+                    { b.buf.size() },
+                    { sizeof(int16_t) }
+                };
+
+});*/
+
+    class_<samples_To_VG>(m,"Samples_To_VG")
             .def(init<int,double,std::vector<int>>())
 //            .def("exec",overload_cast<fexec&, vector_single<double>,vector_single<double>,vector_pair<double>,angle::angle&>(&samples_To_VG::operator()<fexec,vector_single<double>,vector_pair<double>,angle::angle>))
 //            .def("exec",overload_cast<fexec&, vector_single<double>,vector_single<double>,vector_pair<double>,angle::angle&>(&samples_To_VG::operator()))
