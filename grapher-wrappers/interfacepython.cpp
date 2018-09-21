bd7606b8bc3d065c50b6c875b1175a9d63711b38
Fassio Blatter <fassio@specpose.com>
2018-09-21 19:12:21 +0200

    FIXED grapher-wrappers using timecode buffer

--- grapher-wrappers/interfacepython.cpp
+++ grapher-wrappers/interfacepython.cpp
@@ -28,16 +28,52 @@ PYBIND11_MODULE(grapher, m){
 //    pybind11::module m("grapher","");
 //old
 
-    class_<sp::timecode<int16_t>>(m,"TimeCodeInt")
+    class_<sp::timecode<int16_t>>(m,"TimeCodeInt",buffer_protocol())
 	.def(init<int16_t,int16_t>())
+	.def_buffer([](sp::timecode<int16_t>& tc) -> buffer_info{
+	return buffer_info(
+		tc.data(),
+		sizeof(sp::timecode<int16_t>::value_type),
+		format_descriptor<sp::timecode<int16_t>::value_type>::format(),
+		1,
+		{2},
+		{sizeof(sp::timecode<int16_t>::value_type)}
+	);
+	})
 	.def("get_x",&sp::timecode<int16_t>::get_x)
-        .def("get_y",&sp::timecode<int16_t>::get_y)
-	.def(self += self);
-    class_<sp::timecode<double>>(m,"TimeCodeDouble")
+	.def("get_y",&sp::timecode<int16_t>::get_y)
+	.def(self += self)
+	;
+    class_<sp::timecode<double>>(m,"TimeCodeDouble",buffer_protocol())
 	.def(init<double,double>())
+	.def_buffer([](sp::timecode<double>& tc) -> buffer_info{
+	return buffer_info(
+		tc.data(),
+		sizeof(sp::timecode<double>::value_type),
+		format_descriptor<sp::timecode<double>::value_type>::format(),
+		1,
+		{2},
+		{sizeof(sp::timecode<double>::value_type)}
+	);
+	})
 	.def("get_x",&sp::timecode<double>::get_x)
 	.def("get_y",&sp::timecode<double>::get_y)
-	.def(self += self);
+	.def(self += self)
+	;
+    class_<std::vector<sp::timecode<double>>>(m,"VectorTimeCodeDouble",buffer_protocol())
+	.def(init<>())
+	.def_buffer([](std::vector<sp::timecode<double>>& vector) -> buffer_info{
+		return buffer_info(
+			vector.data(),
+			sizeof(std::vector<sp::timecode<double>>::value_type),
+			"",//format_descriptor<sp::timecode<double>>::format(),
+			1,
+			{size_t(vector.size())},
+			{sizeof(sp::timecode<double>)}
+		);
+		})
+	.def("np",[](std::vector<sp::timecode<double>> &vec)->array{return cast(vec);})
+	;
     /*stl
     m.def("VectorInt16",[](std::vector<int16_t> b){return std::vector<int16_t>{b};});
     m.def("VectorTCInt16",[](std::vector<sp::timecode<int16_t>> b){return std::vector<sp::timecode<int16_t>>{b};});
@@ -126,6 +162,8 @@ PYBIND11_MODULE(grapher, m){
     m.def("doublesize",[]()->size_t{return sizeof(std::array<double,9>);});
     m.def("matrixsize",[]()->size_t{return sizeof(Matrix<double>);});
     m.def("arraysize",[]()->size_t{return sizeof(std::array<double,9>);});
+    m.def("tcraw",[]()->size_t{return sizeof(std::array<double,2>);});
+    m.def("tcobject",[]()->size_t{return sizeof(sp::timecode<double>);});
     class_<Stack<double>>(m,"Stack",buffer_protocol())
 	.def(init<>())
 	.def_buffer([](Stack<double>& stack) -> buffer_info{
