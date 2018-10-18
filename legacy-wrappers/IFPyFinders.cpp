8ccfc0abf7c7a7fa202497c84b3844a60a8331c5
Fassio Blatter <fassio@specpose.com>
2018-10-18 20:26:14 +0200

    ADDED findTurns

--- legacy-wrappers/IFPyFinders.cpp
+++ legacy-wrappers/IFPyFinders.cpp
@@ -13,10 +13,26 @@ namespace py = pybind11;
 auto mod = py::module::import("stopeight.grapher");
 
 PYBIND11_MODULE(finders, f){
+
         //vec_tc(f,"VectorTimeCodeDouble",py::buffer_protocol());
         //static QList<dpoint> findTurns(ListCopyable<dpoint> toBeProcessed);
-        f.def("findTurns",[](py::array_t<sp::timecode<double>,py::array::c_style> in)->std::vector<sp::timecode<double>>{
-            return (mod.attr("np_to_tc")(in)).cast<std::vector<sp::timecode<double>>>();
+        f.def("findTurns",[](py::array_t<sp::timecode<double>,py::array::c_style> in)->std::vector<size_t>{
+            //input
+            auto data = (mod.attr("np_to_tc")(in)).cast<std::vector<sp::timecode<double>>>();
+            auto data_qt = QList<dpoint>();
+            //Hack Copy
+            std::transform(std::begin(data),std::end(data),std::back_inserter(data_qt),[](sp::timecode<double>& it_in){
+                return dpoint(qreal(it_in.coords[0]),qreal(it_in.coords[1]));
+            });
+
+            //return
+            auto result_qt =  Turns<dpoint>::findTurns(data_qt);
+            auto result = std::vector<size_t>();
+            //gcc: auto or  windows: decltype
+            std::transform(std::begin(result_qt),std::end(result_qt),std::back_inserter(result),[](dpoint& it_in){
+                return size_t(it_in.position);
+            });
+            return result;
         });
 
 }
