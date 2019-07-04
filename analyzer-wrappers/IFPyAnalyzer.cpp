35c55ecab6bdaea55650fce4fba8a0ce8f1ec0ac
Fassio Blatter <fassio@specpose.com>
2019-07-04 21:18:39 +0200

    FIXED analyzer stub 4

---
+++ analyzer-wrappers/IFPyAnalyzer.cpp
@@ -0,0 +1,12 @@
+// Copyright (C) 2019 Fassio Blatter
+// GNU Lesser General Public License, version 2.1
+#include <stopeight-clibs/analyzer.h>
+
+#include <pybind11/pybind11.h>
+
+#undef NDEBUG
+
+using namespace pybind11;
+
+PYBIND11_MODULE(analyzer, m){
+}
