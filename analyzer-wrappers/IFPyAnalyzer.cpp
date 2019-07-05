30d8ee43c38c3548b6d6b801ece665bb3c3ec7cd
Fassio Blatter <fassio@specpose.com>
2019-07-05 21:17:23 +0200

    FIXED analyzer stub 5

--- analyzer-wrappers/IFPyAnalyzer.cpp
+++ analyzer-wrappers/IFPyAnalyzer.cpp
@@ -1,6 +1,8 @@
 // Copyright (C) 2019 Fassio Blatter
 // GNU Lesser General Public License, version 2.1
 #include <stopeight-clibs/analyzer.h>
+#include <stopeight-clibs/Matrix.h>
+#include <stopeight-clibs/shared_types.h>
 
 #include <pybind11/pybind11.h>
 
@@ -9,4 +11,9 @@
 using namespace pybind11;
 
 PYBIND11_MODULE(analyzer, m){
+	object matrix_module = module::import("stopeight.matrix");
+	m.def("legal_segments", [](Vectors<std::vector<sp::timecode<double>>>& vectors)->Vectors<std::vector<sp::timecode<double>>>&{
+		return vectors;
+    },arg("vectors"));
+
 }
