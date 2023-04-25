079a8c62388514677134688179816bcb7f9b4d8e
Fassio Blatter <fassio@specpose.com>
2023-04-25 11:32:44 +0100

    TEST conda build vs2017, qt5.11.x

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -3,7 +3,7 @@ mkdir build
 cd build
 
 :: Configure using the CMakeFiles
-cmake -G "Visual Studio 17 2022" ^
+cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
