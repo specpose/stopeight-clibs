3afb6f1437dd3c60cb73c426fc17aba475af78c0
Fassio Blatter <fassio@specpose.com>
2023-04-25 11:51:04 +0100

    TEST conda build vs2019, Ninja, qt5.11.x

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,19 +1,16 @@
-:: Make a build folder and change to it.
 mkdir build
 cd build
 
-:: Configure using the CMakeFiles
-cmake -G "NMake Makefiles" ^
-      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_BUILD_TYPE:STRING=Release ^
-      ..
+cmake -G "Ninja" ^
+  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+  -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+  -DCMAKE_SYSTEM_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+  -DCMAKE_BUILD_TYPE="Release" ^
+  ..
 if errorlevel 1 exit 1
 
-:: Build!
-nmake
+ninja -j1
 if errorlevel 1 exit 1
 
-:: Install!
-nmake install
+ninja install
 if errorlevel 1 exit 1
